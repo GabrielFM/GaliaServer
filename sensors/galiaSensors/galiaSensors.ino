eaab5a49a2468759b2bed00715a7ab8f63e65d34
Heitor Rapela <hrm@cin.ufpe.br>
2017-06-16 09:15:07 -0300

    Sensors in arduino with DHT lib

--- sensors/galiaSensors/galiaSensors.ino
+++ sensors/galiaSensors/galiaSensors.ino
@@ -1,11 +1,39 @@
-int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
+#include "DHT.h"
+
+int ldrSensorPin = A0;    
+int ldrSensorValue = 0;
+
+int relaySensorPin = 0;
+int relaySensorValue = 0;
+
+int groundSensorPin = A1;
+int groundSensorValue = 0;
+
+int rainSensorPin = A2;
+int rainSensorValue = 0;
+
+#define DHTPIN A3 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  // Relay - Digital
+  pinMode(relaySensorPin,OUTPUT);
+  dht.begin();
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
+  // LdR
+  ldrSensorValue = analogRead(ldrSensorPin);
+
+  // Ground
+  groundSensorValue = analogRead(groundSensorPin);
+
+  // Relay 
+  digitalWrite(relaySensorPin,relaySensorValue);  
+  
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
   
 }
