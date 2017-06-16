2f1d0c9c5c9bc0873af5544469261a048f4aa7db
Heitor Rapela <hrm@cin.ufpe.br>
2017-06-16 10:42:41 -0300

    Test of all sensors in Arduino style

--- sensors/galiaSensors/galiaSensors.ino
+++ sensors/galiaSensors/galiaSensors.ino
@@ -3,7 +3,7 @@
 int ldrSensorPin = A0;    
 int ldrSensorValue = 0;
 
-int relaySensorPin = 0;
+int relaySensorPin = 2;
 int relaySensorValue = 0;
 
 int groundSensorPin = A1;
@@ -20,20 +20,54 @@ void setup() {
   // Relay - Digital
   pinMode(relaySensorPin,OUTPUT);
   dht.begin();
+
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // LdR
-  ldrSensorValue = analogRead(ldrSensorPin);
+  
+  // LdR // High value = No Light // Low value = Light
+  //ldrSensorValue = analogRead(ldrSensorPin);
+  //delay(200);
+  
+  // Ground // High value = No water // Low value = water
+  //groundSensorValue = analogRead(groundSensorPin);
 
-  // Ground
-  groundSensorValue = analogRead(groundSensorPin);
+  // Rain Sensor // High value = No water // Low value = water
+  // rainSensorValue = analogRead(rainSensorPin);
+  //delay(200);
+
+  //Serial.print("Value Ground: ");
+  //Serial.println(groundSensorValue);
+
+  //Serial.print("Value LDR: ");
+  //Serial.println(ldrSensorValue); 
 
   // Relay 
-  digitalWrite(relaySensorPin,relaySensorValue);  
-  
+  //digitalWrite(relaySensorPin,relaySensorValue);  
 
+  /*
+  Serial.println("LIGAR");
+  relaySensorValue = 1;
+  digitalWrite(relaySensorPin,relaySensorValue);
+  delay(1000);
+  Serial.println("DESLIGAR");
+  relaySensorValue = 0;
+  digitalWrite(relaySensorPin,relaySensorValue);
+  delay(1000);
+  */
+
+ /*
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  
+  delay
+  Serial.print("Value H: ");
+  Serial.print(h); 
+
+  Serial.print(",    T: ");
+  Serial.println(t);
+  */
+  delay(1000);
+
 }
